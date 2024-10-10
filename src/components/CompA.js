16748ccc14a6005e36cb25320c676b39881043ec
Sovan Bakshi <lionalsovan017@gmail.com>
2024-10-10 17:09:19 +0530

    commit

---
+++ src/components/CompA.js
@@ -0,0 +1,23 @@
+import React from 'react'
+
+const CompA = ({item}) => {
+
+    const{title, price, desc, category, img} = item
+
+  return (
+    <div data-test-id={"menu-item-" + category} style={{display:"flex", border:"1px solid black", width:"500px"}}>
+        <img src={img} alt='broken'/>
+
+        <div>
+            <div>
+                <h3>{title}</h3>
+                <span>{price}</span>
+            </div>
+
+            <p>{desc}</p>
+        </div>
+    </div>
+  )
+}
+
+export default CompA
